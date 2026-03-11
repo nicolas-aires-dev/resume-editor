62352d86c059d9d3b0213fc32d6dbf579f767061
Nicolas <ni_colasaires@hotmail.com>
2026-03-11 17:09:40 -0300

    Add Document screen

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import customtkinter as ctk
 from ui.app import TelaInicial
+from docx import Document
 
 
-def processar_docx(caminho):
-    print(f"O usuário escolheu: {caminho}")
-    # aqui você pode chamar funções que leem/processam o docx
+def processar_docx(caminho):    
+    #Open document
+    doc = Document(caminho) 
 
+    props = doc.core_properties
+
+     
 class App(ctk.CTk):
     def __init__(self):
         super().__init__()
-        self.title("App com DOCX")
+        self.title("Resume Editor")
         self.geometry("500x300")
 
         TelaInicial(self, on_submit=processar_docx)
--- ui/app.py
+++ ui/app.py
@@ -1,5 +1,6 @@
 import customtkinter as ctk
 from tkinter import filedialog
+from docx import Document
 
 
 class TelaInicial(ctk.CTkFrame):
@@ -24,3 +25,31 @@ class TelaInicial(ctk.CTkFrame):
         if file_path:
             self.entry.delete(0, "end")
             self.entry.insert(0, file_path)
+
+class TelaDocx(ctk.CTkFrame):
+    def __init__(self, master, caminho):
+        super().__init__(master)
+        self.pack(padx=20, pady=20, fill='both', expand=True)
+
+        #Abertura e processamento de dados
+        doc = Document(caminho)
+        props = doc.core_properties
+
+        #Metadata
+        lbl_title = ctk.CTkLabel(self, text=f"Titúlo: {props.title}")
+        lbl_title.pack(pady=5)
+
+        lbl_author = ctk.CTkLabel(self, text=f"Autor: {props.author}")
+        lbl_author.pack(pady=5)
+
+        lbl_keywords = ctk.CTkLabel(self, text=f"Palavras-chave: {props.keywords}")
+        lbl_keywords.pack(pady=5)
+
+        lbl_description = ctk.CTkLabel(self, text=f"Descrição: {props.comments}")
+        lbl_description.pack(pady=5)
+
+        lbl_category = ctk.CTkLabel(self, text=f"Titúlo: {props.category}")
+        lbl_category.pack(pady=5)
+        
+        self.label = ctk.CTkLabel
+
